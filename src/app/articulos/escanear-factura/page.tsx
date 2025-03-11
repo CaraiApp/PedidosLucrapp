ad869fcaa74bcd188268c1d39952926110cffb0b
caraiapp <info@caraimelilla.com>
2025-03-11 13:59:55 +0100

    Descripción de los cambios

--- src/app/articulos/escanear-factura/page.tsx
+++ src/app/articulos/escanear-factura/page.tsx
@@ -920,21 +920,21 @@ function EscanerFactura() {
               {datosEscaneados.articulos.length === 0 ? (
                 <p className="text-gray-500">No se han detectado artículos en la factura.</p>
               ) : (
-                <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4">
-                  <div className="flex">
-                    <div className="flex-shrink-0">
-                      <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9z" clipRule="evenodd" />
-                      </svg>
-                    </div>
-                    <div className="ml-3">
-                      <p className="text-sm text-yellow-700">
-                        Los nombres de artículos y precios se han extraído automáticamente. Verifica y corrige si es necesario para asegurar datos precisos.
-                      </p>
+                <div>
+                  <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4">
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9z" clipRule="evenodd" />
+                        </svg>
+                      </div>
+                      <div className="ml-3">
+                        <p className="text-sm text-yellow-700">
+                          Los nombres de artículos y precios se han extraído automáticamente. Verifica y corrige si es necesario para asegurar datos precisos.
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <>
                   {/* Tabla para pantallas medianas y grandes */}
                   <div className="hidden md:block overflow-x-auto">
                     <table className="min-w-full divide-y divide-gray-200">
@@ -1096,7 +1096,7 @@ function EscanerFactura() {
                       </div>
                     ))}
                   </div>
-                </>
+                </div>
               )}
               
               <div className="mt-6 flex justify-end">
